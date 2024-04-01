110ada1c3d26ec814baf1a99a8cad2ee0ce8a03e
aditya <jaditya98@gmail.com>
2024-04-01 19:34:00 +0530

    add mobile nav and main nav for responsiveness

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
+import MainNav from "./MainNav";
 
 const Header = () => {
   return (
@@ -8,8 +10,16 @@ const Header = () => {
           to="/"
           className="text-3xl font-bold tracking-tight text-orange-500"
         >
-          Swiggy
+          Aaharly
         </Link>
+
+        <div className="md:hidden">
+          <MobileNav />
+        </div>
+
+        <div className="hidden md:block">
+          <MainNav />
+        </div>
       </div>
     </div>
   );
