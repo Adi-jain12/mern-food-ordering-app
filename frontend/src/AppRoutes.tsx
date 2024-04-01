af8e75b8e122224a7c3237e38a4940a4cdb29db2
aditya <jaditya98@gmail.com>
2024-04-01 18:13:14 +0530

    connect database and setup routes in frontend

---
+++ frontend/src/AppRoutes.tsx
@@ -0,0 +1,14 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<span>HOME PAGE</span>} />
+      <Route path="/user-profile" element={<span>USER PROFILE</span>} />
+
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
