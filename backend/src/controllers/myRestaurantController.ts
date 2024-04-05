3f853bb4783a71f1ebc8bdbe93b6e63a4505212c
aditya <jaditya98@gmail.com>
2024-04-05 23:59:50 +0530

    fix bug and change restaurant api logic

--- backend/src/controllers/myRestaurantController.ts
+++ backend/src/controllers/myRestaurantController.ts
@@ -3,14 +3,23 @@ import Restaurant from "../models/restaurant";
 
 export const createRestaurant = async (req: Request, res: Response) => {
   try {
-    const user = await Restaurant.findById(req.userId);
+    const existingRestaurant = await Restaurant.findOne({ user: req.userId });
 
-    if (!user) {
-      return res.status(401).json({ message: "User not found" });
+    if (existingRestaurant) {
+      return res
+        .status(409)
+        .json({ message: "User restaurant already exists" });
     }
 
+    const imageUrl = await uploadImage(req.file as Express.Multer.File);
+
     const restaurant = new Restaurant(req.body);
+    restaurant.imageUrl = imageUrl;
+    restaurant.user = new mongoose.Types.ObjectId(req.userId);
+    restaurant.lastUpdated = new Date();
     await restaurant.save();
+
+    res.status(201).send(restaurant);
   } catch (error) {
     res.status(500).json({ message: "Error creating restaurant" });
   }
