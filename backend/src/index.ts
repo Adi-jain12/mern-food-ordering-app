9fcb2225c0f1228d9f213d56ba7647066356005e
aditya <jaditya98@gmail.com>
2024-04-05 19:36:34 +0530

    set tsconfig file for deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,6 +12,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+//extra sanity check to check if the server has successfully started and we can make requests to the endpoints
+
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" }); //this convention is used by services like docker compose and kubernetes which they will call this /health endpoint and if they receive a valid response then it will assume that, that service is healthy.
+});
+
 app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, () => {
