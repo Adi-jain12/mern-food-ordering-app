1068755c25b79296ac518a38738c77d2216a1d74
aditya <jaditya98@gmail.com>
2024-04-05 23:56:56 +0530

    add multer config

--- backend/src/routes/myRestaurantRoutes.ts
+++ backend/src/routes/myRestaurantRoutes.ts
@@ -1,9 +1,24 @@
 import express from "express";
 import { jwtCheck, jwtParse } from "../middleware/auth";
 import { createRestaurant } from "../controllers/myRestaurantController";
+import multer from "multer";
 
 const router = express.Router();
 
-router.post("/", jwtCheck, jwtParse, createRestaurant);
+const storage = multer.memoryStorage(); //stores any files i.e images from post request in memory not locally because getting the images and storing directly in cloudinary
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB
+  },
+});
+
+router.post(
+  "/",
+  jwtCheck,
+  jwtParse,
+  upload.single("imageFile"),
+  createRestaurant
+);
 
 export default router;
