e3348cd4e0ce79117b28d69035da7e783f207f4b
aditya <jaditya98@gmail.com>
2024-04-08 23:54:45 +0530

    add menu section in form

---
+++ frontend/src/forms/manage-restaurant-form/MenuItemInput.tsx
@@ -0,0 +1,57 @@
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useFormContext } from "react-hook-form";
+
+type Props = {
+  index: number;
+  removeMenuItem: () => void;
+};
+
+const menuItemInput = ({ index, removeMenuItem }: Props) => {
+  const { control } = useFormContext();
+
+  return (
+    <div className="flex flex-row items-end gap-2">
+      <FormField
+        control={control}
+        name={`menuItems.${index}.name`}
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel className="flex items-center gap-1">
+              Name <FormMessage />
+            </FormLabel>
+            <FormControl>
+              <Input
+                {...field}
+                placeholder="Cheese Pizza"
+                className="bg-white"
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={control}
+        name={`menuItems.${index}.price`}
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel className="flex items-center gap-1">
+              Price (₹) <FormMessage />
+            </FormLabel>
+            <FormControl>
+              <Input {...field} placeholder="8.00" className="bg-white" />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+    </div>
+  );
+};
+
+export default menuItemInput;
---
+++ frontend/src/forms/manage-restaurant-form/MenuSection.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/ui/button";
+import { FormDescription, FormField, FormItem } from "@/components/ui/form";
+import { useFieldArray, useFormContext } from "react-hook-form";
+
+const MenuSection = () => {
+  const { control } = useFormContext();
+
+  // fields will be an array (i.e menuItems) of objects and each object will have name and price in it, append : is used to add new menuItem in the array, remove : is used to remove the menuItem from the array
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "menuItems",
+  });
+
+  return (
+    <div className="space-y-2">
+      <div>
+        <h2 className="font-2xl font-bold">Menu</h2>
+        <FormDescription>
+          Create you menu and give item a name and a price
+        </FormDescription>
+      </div>
+
+      <FormField
+        control={control}
+        name="menuItems"
+        render={() => (
+          <FormItem className="flex flex-col gap-2">
+            {fields.map((_, index) => (
+              <MenuItemInput
+                index={index}
+                removeMenuItem={() => remove(index)}
+              />
+            ))}
+          </FormItem>
+        )}
+      />
+      <Button type="submit" onClick={() => append({ name: "", price: "" })}>
+        Add Menu Item
+      </Button>
+    </div>
+  );
+};
+
+export default MenuSection;
